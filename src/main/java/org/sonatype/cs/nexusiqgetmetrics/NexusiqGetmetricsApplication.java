1a500dddc31cdf864757749c468750a7686f4a14
Sola <solao@novi.local>
2022-02-10 13:13:43 +0000

    1

---
+++ src/main/java/org/sonatype/cs/nexusiqgetmetrics/NexusiqGetmetricsApplication.java
@@ -0,0 +1,13 @@
+package org.sonatype.cs.nexusiqgetmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusiqGetmetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusiqGetmetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sonatype/cs/nexusiqgetmetrics/NexusiqGetmetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sonatype.cs.nexusiqgetmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusiqGetmetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
